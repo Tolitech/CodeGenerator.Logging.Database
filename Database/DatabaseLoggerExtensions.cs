57a2687b1c02244011be21b92016dcd28bef746d
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2020-06-04 11:59:35 -0300

    Repositório inicial.

---
+++ Database/DatabaseLoggerExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Tolitech.CodeGenerator.Logging.Database
+{
+    public static class DatabaseLoggerExtensions
+    {
+        static public ILoggingBuilder AddDatabaseLogger(this ILoggingBuilder builder)
+        {
+            builder.AddConfiguration();
+
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, DatabaseLoggerProvider>());
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<DatabaseLoggerOptions>, DatabaseLoggerOptionsSetup>());
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IOptionsChangeTokenSource<DatabaseLoggerOptions>, LoggerProviderOptionsChangeTokenSource<DatabaseLoggerOptions, DatabaseLoggerProvider>>());
+            return builder;
+        }
+
+        static public ILoggingBuilder AddDatabaseLogger(this ILoggingBuilder builder, Action<DatabaseLoggerOptions> configure)
+        {
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            builder.AddDatabaseLogger();
+            builder.Services.Configure(configure);
+
+            return builder;
+        }
+    }
+}
---
+++ Database/DatabaseLoggerOptionsSetup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Tolitech.CodeGenerator.Logging.Database
+{
+    internal class DatabaseLoggerOptionsSetup : ConfigureFromConfigurationOptions<DatabaseLoggerOptions>
+    {
+        public DatabaseLoggerOptionsSetup(ILoggerProviderConfiguration<DatabaseLoggerProvider> providerConfiguration) : base(providerConfiguration.Configuration)
+        {
+
+        }
+    }
+}
